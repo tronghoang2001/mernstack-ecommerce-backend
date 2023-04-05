f72d7d36a3c74ea6acef96f5e222777dba673127
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-05 21:15:51 +0700

    phân trang

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,21 @@
-const getProducts = (req, res) => {
-  res.send("Handling product routes, e.g. search for products");
+const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
+
+const getProducts = async (req, res, next) => {
+  try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+    res.json({
+      products,
+      pageNum,
+      paginationLinkNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 module.exports = getProducts;
