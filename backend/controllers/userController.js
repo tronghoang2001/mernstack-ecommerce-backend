2afa99395d6bc0112472665058d0789d969c6703
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 17:10:25 +0700

    dang nhap

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel");
-const { hashPassword } = require("../utils/hashPassword");
+const { hashPassword, comparePasswords } = require("../utils/hashPassword");
+const generateAuthToken = require("../utils/generateAuthToken");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -19,7 +20,7 @@ const registerUser = async (req, res, next) => {
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(400).json({ error: "user exists" });
+      return res.status(400).send("user exists");
     } else {
       const hashedPassword = hashPassword(password);
       const user = await User.create({
@@ -28,11 +29,87 @@ const registerUser = async (req, res, next) => {
         email: email.toLowerCase(),
         password: hashedPassword,
       });
-      res.status(201).send(user);
+      res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+          }
+        )
+        .status(201)
+        .json({
+          success: "User created",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
     }
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { getUsers, registerUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, doNotLogout } = req.body;
+    if (!(email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const user = await User.findOne({ email });
+    if (user && comparePasswords(password, user.password)) {
+      let cookieParams = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      };
+
+      if (doNotLogout) {
+        cookieParams = { ...cookieParams, maxAge: 1000 * 60 * 60 * 24 * 7 }; // 1000=1ms
+      }
+
+      return res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          cookieParams
+        )
+        .json({
+          success: "user logged in",
+          userLoggedIn: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            doNotLogout,
+          },
+        });
+    } else {
+      return res.status(401).send("wrong credentials");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser, loginUser };
