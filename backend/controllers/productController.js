15b39612a0b60978179d69730c3d625e1bc5e604
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 16:20:06 +0700

    them, sua san pham

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/ProductModel");
 const recordsPerPage = require("../config/pagination");
+const imageValidate = require("../utils/imageValidate");
 
 const getProducts = async (req, res, next) => {
   try {
@@ -156,10 +157,148 @@ const adminDeleteProduct = async (req, res, next) => {
   }
 };
 
+const adminCreateProduct = async (req, res, next) => {
+  try {
+    const product = new Product();
+    const {
+      name,
+      description,
+      count,
+      price,
+      category,
+      attributesTable,
+    } = req.body;
+    product.name = name;
+    product.description = description;
+    product.count = count;
+    product.price = price;
+    product.category = category;
+    if (attributesTable.length > 0) {
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    }
+    await product.save();
+
+    res.json({
+      message: "product created",
+      productId: product._id,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminUpdateProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    const {
+      name,
+      description,
+      count,
+      price,
+      category,
+      attributesTable,
+    } = req.body;
+    product.name = name || product.name;
+    product.description = description || product.description;
+    product.count = count || product.count;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    if (attributesTable.length > 0) {
+      product.attrs = [];
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    } else {
+      product.attrs = [];
+    }
+    await product.save();
+    res.json({
+      message: "product updated",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminUpload = async (req, res, next) => {
+  try {
+    if (!req.files || !!req.files.images === false) {
+      return res.status(400).send("No files were uploaded.");
+    }
+
+    const validateResult = imageValidate(req.files.images);
+    if (validateResult.error) {
+      return res.status(400).send(validateResult.error);
+    }
+
+    const path = require("path");
+    const { v4: uuidv4 } = require("uuid");
+    const uploadDirectory = path.resolve(
+      __dirname,
+      "../../frontend",
+      "public",
+      "images",
+      "products"
+    );
+
+    let product = await Product.findById(req.query.productId).orFail();
+
+    let imagesTable = [];
+    if (Array.isArray(req.files.images)) {
+      imagesTable = req.files.images;
+    } else {
+      imagesTable.push(req.files.images);
+    }
+
+    for (let image of imagesTable) {
+      var fileName = uuidv4() + path.extname(image.name);
+      var uploadPath = uploadDirectory + "/" + fileName;
+      product.images.push({ path: "/images/products/" + fileName });
+      image.mv(uploadPath, function(err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
+      });
+    }
+    await product.save();
+    return res.send("Files uploaded!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminDeleteProductImage = async (req, res, next) => {
+  try {
+    const imagePath = decodeURIComponent(req.params.imagePath);
+    const path = require("path");
+    const finalPath = path.resolve("../frontend/public") + imagePath;
+
+    const fs = require("fs");
+    fs.unlink(finalPath, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+    await Product.findOneAndUpdate(
+      { _id: req.params.productId },
+      { $pull: { images: { path: imagePath } } }
+    ).orFail();
+    return res.end();
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getProducts,
   getProductById,
   getBestsellers,
   adminGetProducts,
   adminDeleteProduct,
+  adminCreateProduct,
+  adminUpdateProduct,
+  adminUpload,
+  adminDeleteProductImage,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,10 @@ const {
   getBestsellers,
   adminGetProducts,
   adminDeleteProduct,
+  adminCreateProduct,
+  adminUpdateProduct,
+  adminUpload,
+  adminDeleteProductImage,
 } = require("../controllers/productController");
 
 router.get("/category/:categoryName/search/:searchQuery", getProducts);
@@ -18,5 +22,9 @@ router.get("/get-one/:id", getProductById);
 // admin routes:
 router.get("/admin", adminGetProducts);
 router.delete("/admin/:id", adminDeleteProduct);
+router.delete("/admin/image/:imagePath/:productId", adminDeleteProductImage);
+router.put("/admin/:id", adminUpdateProduct);
+router.post("/admin/upload", adminUpload);
+router.post("/admin", adminCreateProduct);
 
 module.exports = router;
