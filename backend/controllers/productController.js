0b62088186be4c23e0246964d18472d596faca0f
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 12:51:59 +0700

    lay san pham ban chay nhat

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -117,4 +117,22 @@ const getProductById = async (req, res, next) => {
   }
 };
 
-module.exports = { getProducts, getProductById };
+const getBestsellers = async (req, res, next) => {
+  try {
+    const products = await Product.aggregate([
+      { $sort: { category: 1, sales: -1 } },
+      {
+        $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } },
+      },
+      { $replaceWith: "$doc_with_max_sales" },
+      { $match: { sales: { $gt: 0 } } },
+      { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
+      { $limit: 3 },
+    ]);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getProducts, getProductById, getBestsellers };
