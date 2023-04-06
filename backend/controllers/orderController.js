8eb8d64289fbc600c35b563e77cfaea391d727db
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 18:58:11 +0700

    user lay ds, chi tiet, tao moi ddh

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,59 @@
-const getOrders = (req, res) => {
-  res.send("Handling order routes, e.g. get orders");
+const Order = require("../models/OrderModel");
+const Product = require("../models/ProductModel");
+const ObjectId = require("mongodb").ObjectId;
+
+const getUserOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find({ user: ObjectId(req.user._id) });
+    res.send(orders);
+  } catch (error) {
+    next(error);
+  }
 };
-module.exports = getOrders;
+
+const getOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id)
+      .populate("user", "-password -isAdmin -_id -__v -createdAt -updatedAt")
+      .orFail();
+    res.send(order);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const createOrder = async (req, res, next) => {
+  try {
+    const { cartItems, orderTotal, paymentMethod } = req.body;
+    if (!cartItems || !orderTotal || !paymentMethod) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    let ids = cartItems.map((item) => {
+      return item.productID;
+    });
+    let qty = cartItems.map((item) => {
+      return Number(item.quantity);
+    });
+
+    await Product.find({ _id: { $in: ids } }).then((products) => {
+      products.forEach(function(product, idx) {
+        product.sales += qty[idx];
+        product.save();
+      });
+    });
+
+    const order = new Order({
+      user: ObjectId(req.user._id),
+      orderTotal: orderTotal,
+      cartItems: cartItems,
+      paymentMethod: paymentMethod,
+    });
+    const createdOrder = await order.save();
+    res.status(201).send(createdOrder);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUserOrders, getOrder, createOrder };
