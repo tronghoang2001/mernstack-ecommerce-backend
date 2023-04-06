96718c7fef4940801df6ffdf36ac4844554afcbc
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 16:41:02 +0700

    dang ky tai khoan

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,38 @@
-const getUsers = (req, res) => {
-  res.send("Handling user routes, e.g. get users");
+const User = require("../models/UserModel");
+const { hashPassword } = require("../utils/hashPassword");
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}).select("-password");
+    return res.json(users);
+  } catch (err) {
+    next(err);
+  }
 };
-module.exports = getUsers;
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "user exists" });
+    } else {
+      const hashedPassword = hashPassword(password);
+      const user = await User.create({
+        name,
+        lastName,
+        email: email.toLowerCase(),
+        password: hashedPassword,
+      });
+      res.status(201).send(user);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser };
