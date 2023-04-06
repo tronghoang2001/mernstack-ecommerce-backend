bd283e916220a828fbaad9813ae9f1f785c355db
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-06 19:24:45 +0700

    ddh - cap nhat trang thai, ds admin, lay du lieu cho bieu do

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -56,4 +56,67 @@ const createOrder = async (req, res, next) => {
   }
 };
 
-module.exports = { getUserOrders, getOrder, createOrder };
+const updateOrderToPaid = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id).orFail();
+    order.isPaid = true;
+    order.paidAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.send(updatedOrder);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateOrderToDelivered = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id).orFail();
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    const updatedOrder = await order.save();
+    res.send(updatedOrder);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find({})
+      .populate("user", "-password")
+      .sort({ paymentMethod: "desc" });
+    res.send(orders);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getOrderForAnalysis = async (req, res, next) => {
+  try {
+    const start = new Date(req.params.date);
+    start.setHours(0, 0, 0, 0);
+    const end = new Date(req.params.date);
+    end.setHours(23, 59, 59, 999);
+
+    const order = await Order.find({
+      createdAt: {
+        $gte: start,
+        $lte: end,
+      },
+    }).sort({ createdAt: "asc" });
+    res.send(order);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getUserOrders,
+  getOrder,
+  createOrder,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+  getOrderForAnalysis,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -8,6 +8,10 @@ const {
   getUserOrders,
   getOrder,
   createOrder,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+  getOrderForAnalysis,
 } = require("../controllers/orderController");
 
 // user routes
@@ -15,8 +19,12 @@ router.use(verifyIsLoggedIn);
 router.get("/", getUserOrders);
 router.get("/user/:id", getOrder);
 router.post("/", createOrder);
+router.put("/paid/:id", updateOrderToPaid);
 
 // admin routes
 router.use(verifyIsAdmin);
+router.put("/delivered/:id", updateOrderToDelivered);
+router.get("/admin", getOrders);
+router.get("/analysis/:date", getOrderForAnalysis);
 
 module.exports = router;
