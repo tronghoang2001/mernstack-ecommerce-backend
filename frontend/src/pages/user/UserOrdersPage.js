05e89cfc5ecc150b876f7956157e88aad988141a
Hoàng Nguyễn <12a9nth@gmail.com>
2023-04-08 12:45:58 +0700

    user list orders, edit product

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,46 +1,13 @@
-import { Row, Col, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import UserOrdersPageComponent from "./components/UserOrdersPageComponent";
+import axios from "axios";
+
+const getOrders = async () => {
+  const { data } = await axios.get("/api/orders");
+  return data;
+};
 
 const UserOrdersPage = () => {
-  return (
-    <Row className="m-5">
-      <Col md={12}>
-        <h1>My Orders</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr style={{ backgroundColor: "#ffc107" }}>
-              <th>#</th>
-              <th>User</th>
-              <th>Date</th>
-              <th>Total</th>
-              <th>Delivered</th>
-              <th>Order details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {[
-              "bi bi-check-lg text-success",
-              "bi bi-x-lg text-danger",
-              "bi bi-check-lg text-success",
-            ].map((item, idx) => (
-              <tr key={idx}>
-                <td>{idx + 1}1</td>
-                <td>Hoang Nguyen</td>
-                <td>16-01-2023</td>
-                <td>$124</td>
-                <td>
-                  <i className={item}></i>
-                </td>
-                <td>
-                  <Link to="/user/order-details">Go to order</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
-  );
+  return <UserOrdersPageComponent getOrders={getOrders} />;
 };
 
 export default UserOrdersPage;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,10 +4,12 @@ import thunk from "redux-thunk";
 
 import { cartReducer } from "./reducers/cartReducers";
 import { userRegisterLoginReducer } from "./reducers/userReducers";
+import { getCategoriesReducer } from "./reducers/categoryReducers";
 
 const reducer = combineReducers({
   cart: cartReducer,
   userRegisterLogin: userRegisterLoginReducer,
+  getCategories: getCategoriesReducer,
 });
 
 const cartItemsInLocalStorage = localStorage.getItem("cart")
